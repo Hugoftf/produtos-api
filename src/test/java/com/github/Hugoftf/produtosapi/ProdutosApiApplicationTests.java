14107a8b08613e71ef03e30e2e91946a2e9f4af7
Hugo de França Torres Ferreira <f3rreirahugo@hotmail.com>
2025-02-16 20:23:47 -0300

    iniciando projeto primeiro commit

---
+++ src/test/java/com/github/Hugoftf/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Hugoftf.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
